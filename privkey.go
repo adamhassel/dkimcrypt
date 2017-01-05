747740c9f6077a22744f1f8c4733f6c19ef421cb
Adam Hasselbalch Hansen <ahh@one.com>
2017-01-05 09:54:17 +0100

    use declared constants as key types

--- privkey.go
+++ privkey.go
@@ -8,6 +8,9 @@ import (
 	"io/ioutil"
 )
 
+const PRIVKEY = "PRIVATE KEY"
+const RSAKEY = "RSA PRIVATE KEY"
+
 func getPrivKeyFromFile(filename string) (key *rsa.PrivateKey, err error) {
 	var pemData []byte
 	var block *pem.Block
@@ -24,18 +27,18 @@ func getPrivKeyFromFile(filename string) (key *rsa.PrivateKey, err error) {
 	}
 
 	switch block.Type {
-	case "PRIVATE KEY":
+	case PRIVKEY:
 		var tmp interface{}
 		if tmp, err = x509.ParsePKCS8PrivateKey(block.Bytes); err != nil {
 			return nil, fmt.Errorf("Bad private key: %s", err)
 		}
 		privkey = tmp.(*rsa.PrivateKey)
-	case "RSA PRIVATE KEY":
+	case RSAKEY:
 		if privkey, err = x509.ParsePKCS1PrivateKey(block.Bytes); err != nil {
 			return nil, fmt.Errorf("Bad private key: %s", err)
 		}
 	default:
-		return nil, fmt.Errorf("Unknown key type '%s', want either '%s' or '%s'", block.Type, "PRIVATE KEY", "RSA PRIVATE KEY")
+		return nil, fmt.Errorf("Unknown key type '%s', want either '%s' or '%s'", block.Type, PRIVKEY, RSAKEY)
 	}
 
 	return privkey, nil
